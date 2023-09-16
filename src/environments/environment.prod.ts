e2cb239fa158c5a00e3ce084bc93361f2f945f68
Taco Belle <drusz92@gmail.com>
2023-09-16 11:45:17 -0400

    Version 1.0 with new api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7076'
+  apiUrl: 'https://poketrainerapi.azurewebsites.net/'
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7076'
+  apiUrl: 'https://poketrainerapi.azurewebsites.net/'
 };
 
 /*

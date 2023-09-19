3f97ae4561aa8c6c3e6e82c95e6d8c898d3e545b
Taco Belle <drusz92@gmail.com>
2023-09-19 18:15:40 -0400

    Fixing crit

--- src/app/components/play-right-box/play-right-box.component.ts
+++ src/app/components/play-right-box/play-right-box.component.ts
@@ -245,13 +245,13 @@ getEncounter() {
     ).subscribe(({ data, creatureData }) => {
       const creature = creatureData[0] ?? new Creature();
       this.creature = creature;
-      if (this.creature.currentHealth > (creature.currentHealth ?? 0) + 4) {
+      if (this.creature.currentHealth > (creature.currentHealth ?? 0) + 2) {
         this.critEvent.emit();
       }
       if (this.creature.id !== 0) {
         if (data && data.length > 0) {
           const encounterHealth = data[0].currentHealth; 
-          if (this.encounter.currentHealth > encounterHealth + 4) {
+          if (this.encounter.currentHealth > encounterHealth + 2) {
             this.critHit();
           }
           this.encounter.currentHealth = encounterHealth;
